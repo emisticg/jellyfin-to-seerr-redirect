7bde88182556c8c3663330306a2f4179fe4c6ef1
Mateusz Tasz <emisticg@gmail.com>
2026-05-08 14:19:09 +0200

    Update redirect.js
    Dodano obsługę menu nawigacyjnego

--- redirect.js
+++ redirect.js
@@ -1,13 +1,12 @@
-<script>
     document.addEventListener('click', function(event) {
         // Szukamy najbliższego kafelka (karty)
-        const card = event.target.closest('.card');
+        const card = event.target.closest('.card, .navMenuOption');
 
         if (card) {
-            // Sprawdzamy po unikalnym ID Twojej biblioteki "Discover"
-            const itemId = card.getAttribute('data-id');
+            // Sprawdzamy po unikalnym ID Twojej biblioteki
+            const itemId = card.getAttribute('data-id') || card.getAttribute('data-itemid');
 
-            // ID z Twojego kodu to: 019e0750d3ff75a79b877af453d36efb zamień na swój
+            // ID z kodu to: 019e0750d3ff75a79b877af453d36efb zamień na swój
             if (itemId === '019e0750d3ff75a79b877af453d36efb') {
 
                 console.log("Wykryto kliknięcie w bibliotekę przekierowań. Przekierowuję do Jellyseerr...");
@@ -17,9 +16,7 @@
 
                 // TU WPISZ SWÓJ ADRES JELLYSEERR
                 const jellyseerrUrl = 'https://127.0.0.1:5055';
-
                 window.location.href = jellyseerrUrl;
             }
         }
     }, true);
-</script>
